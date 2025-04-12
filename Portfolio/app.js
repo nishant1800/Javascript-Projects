a84549e93052dcc3d7585a4ead31149d7d6ebb62
NishantRanjan <nishantcodes18@gmail.com>
2025-04-13 03:48:14 +0530

    add some files

---
+++ Portfolio/app.js
@@ -0,0 +1,22 @@
+// navbar code
+
+let navH1 = document.querySelector(".nav-h1");
+let body = document.querySelector("body");
+let navOptions = document.querySelector(".nav-options");
+let navBtn = document.querySelector(".nav-btn");
+
+navH1.addEventListener("click", function() {
+    body.style.backgroundColor = "black";
+     navH1.style.display = "none";
+     navOptions.style.display = "none";
+     navBtn.style.display = "none";
+     body.innerHTML = `<h1>PORTFOLIO</h1>`
+
+    body.style.color = "palegreen";
+    body.style.display = "flex";
+    body.style.alignItems = "center";
+    body.style.justifyContent = "center";
+    body.style.fontSize = "80px"
+
+
+})
