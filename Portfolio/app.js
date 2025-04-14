469d7e38245aaf2edcc68c0b87879db82229c785
NishantRanjan <nishantcodes18@gmail.com>
2025-04-14 19:05:56 +0530

    undo

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -5,7 +5,7 @@ let body = document.querySelector("body");
 let navOptions = document.querySelector(".nav-options");
 let navBtn = document.querySelector(".nav-btn");
 
-navH1.addEventListener("click", function() {
+navH1.addEventListener("mouseover", function() {
     body.style.backgroundColor = "black";
      navH1.style.display = "none";
      navOptions.style.display = "none";
@@ -17,6 +17,19 @@ navH1.addEventListener("click", function() {
     body.style.alignItems = "center";
     body.style.justifyContent = "center";
     body.style.fontSize = "80px"
+})
 
+// herosection code
 
+let herosectionRightMyImage = document.querySelector(".herosection-right-myimage");
+
+let originalSrc = "nishant.png";
+let changeSrc = "nishant2.png";
+
+herosectionRightMyImage.addEventListener("click", function() {
+    if(herosectionRightMyImage.src.includes(originalSrc)) {
+        herosectionRightMyImage.src = changeSrc;
+    } else {
+        herosectionRightMyImage.src = originalSrc
+    }
 })
