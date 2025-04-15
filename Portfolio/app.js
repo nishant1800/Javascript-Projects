295e196d911d1040de9278db697b90b1958dd099
NishantRanjan <nishantcodes18@gmail.com>
2025-04-15 22:21:20 +0530

    add about

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -33,3 +33,27 @@ herosectionRightMyImage.addEventListener("click", function() {
         herosectionRightMyImage.src = originalSrc
     }
 })
+
+
+// about section
+let aboutRightList = document.querySelector(".about-right-skills");
+let aboutRightSkills = document.querySelector(".about-right-list");
+let aboutRightEducation = document.querySelector(".about-right-education");
+
+let aboutRightList1 = document.querySelector(".about-right-list-1");
+let aboutRightList2 = document.querySelector(".about-right-list-2");
+
+aboutRightList1.style.display="none";
+aboutRightList2.style.display="block";
+
+aboutRightSkills.addEventListener("click", function() {
+    aboutRightList1.style.display="block";
+    aboutRightList2.style.display="none";
+})
+
+aboutRightEducation.addEventListener("click", function() {
+    aboutRightList2.style.display="block";
+    aboutRightList1.style.display="none";
+})
+
+// services section
