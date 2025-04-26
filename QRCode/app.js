34920b8d5a978d8440edd347350d2e761599ad67
nishant1800 <nishantcodes18@gmail.com>
2025-04-26 23:56:59 +0530

    qr

---
+++ QRCode/app.js
@@ -0,0 +1,8 @@
+
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("quImage");
+let qrText = document.getElementById("qrText");
+
+function generateQR() {
+    
+}
